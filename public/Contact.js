80c30ebcf9030065d0a5e8278b2a76a001f25b1f
Bishal Saha <bishalboss557@gmail.com>
2020-08-21 23:59:11 +0530

    Updated

--- public/Contact.js
+++ public/Contact.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCBhLlik0YD-nP9IkdMmxNeUnXzAv3A-Cs",
-  authDomain: "boring-men.firebaseapp.com",
-  databaseURL: "https://boring-men.firebaseio.com",
-  projectId: "boring-men",
-  storageBucket: "boring-men.appspot.com",
-  messagingSenderId: "956670404039",
-  appId: "1:956670404039:web:a5c928c9707af6d38965e2",
-  measurementId: "G-1LT5NFKT0B"
+  apiKey: "xxx",
+  authDomain: "xxx",
+  databaseURL: "xxx",
+  projectId: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx",
+  measurementId: "xxx"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
